91c4a746e5ae88819e49ca7bd627833ca4551ca2
Natsuki Endo <endo.natsuki.kb@un.tsukuba.ac.jp>
2023-11-13 08:53:28 +0900

    ゲームジャム終了後の修正をするために、ブランチを作成しました

--- SandCastle/Main.cpp
+++ SandCastle/Main.cpp
@@ -1,15 +1,12 @@
-﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.10
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.12
 
 // シーンの名前
 enum class State
 {
 	Title,
 	SandCastle,
-	Game,
 	Ranking,
 };
-
-
 
 // 共有するデータ
 struct GameData
@@ -81,10 +78,10 @@ public:
 	//描写だけ
 	void draw() const override
 	{
-		Scene::SetBackground(ColorF{ 0.2, 0.8, 0.4 });
+		//背景
+		Scene::Rect().draw(Arg::top = ColorF{ 0.2, 0.5, 1.0 }, Arg::bottom = ColorF{ 0.5, 0.8, 1.0 });
 
-		FontAsset(U"TitleFont")(U"Sand Castle")
-			.drawAt(TextStyle::OutlineShadow(0.2, ColorF{ 0.2, 0.6, 0.2 }, Vec2{ 3, 3 }, ColorF{ 0.0, 0.5 }), 100, Vec2{ 400, 100 });
+		FontAsset(U"TitleFont")(U"Sand Castle").drawAt(100, Vec2{ 400, 100 });
 
 		m_startButton.draw(ColorF{ 1.0, m_startTransition.value() }).drawFrame(2);
 		m_rankingButton.draw(ColorF{ 1.0, m_rankingTransition.value() }).drawFrame(2);
@@ -94,7 +91,6 @@ public:
 		FontAsset(U"Menu")(U"RANKING").drawAt(m_rankingButton.center(), ColorF{ 0.25 });
 		FontAsset(U"Menu")(U"EXIT").drawAt(m_exitButton.center(), ColorF{ 0.25 });
 
-		FontAsset(U"TitleFont")(U"□をクリックしてスタート").drawAt(30, Vec2{ 400, 400 });
 	}
 
 private:
@@ -108,7 +104,7 @@ private:
 	Rect m_exitButton{ Arg::center = Scene::Center().movedBy(0, 200), 300, 60 };
 	Transition m_exitTransition{ 0.4s, 0.2s };
 
-	double volume = 0.2;
+	double volume = 1.0;
 
 };
 
@@ -123,16 +119,19 @@ public:
 		getData().sandmap.resize(m_height, m_width, 0);
 
 		//スコップの設定
-		scoops << Scoop{ Scoopname::Mini, Palette::White, U"1マスだけ掘れるよ", 1 };
-		scoops << Scoop{ Scoopname::Standard, Palette::Black, U"4マス掘れるよ", 2 };
-		scoops << Scoop{ Scoopname::Miduim, Palette::Brown, U"9マス掘れるよ", 3 };
-		scoops << Scoop{ Scoopname::Big, Palette::Pink, U"25マス掘れるよ", 4 };
+		scoops << Scoop{ Scoopname::Mini, Texture{U"NewAssets/photo/mini.png"}, U"1マスだけ掘れるよ", 1 };
+		scoops << Scoop{ Scoopname::Standard, Texture{U"NewAssets/photo/standard.png"}, U"4マス掘れるよ", 2 };
+		scoops << Scoop{ Scoopname::Miduim, Texture{U"NewAssets/photo/medium.png"}, U"9マス掘れるよ", 3 };
+		scoops << Scoop{ Scoopname::Big, Texture{U"NewAssets/photo/mini.png"}, U"25マス掘れるよ", 4 };
 
+		//実装が間に合わなかった
+		//scoops << Scoop{ Scoopname::Cross, Texture{U"NewAssets/photo/mini.png"}, U"十字に5マス掘れるよ", 4 };
+		//scoops << Scoop{ Scoopname::triangle, Texture{U"NewAssets/photo/mini.png"}, U"三角に6マス掘れるよ", 4 };
 
 		//初期スコップの選択&アイテム欄に追加
-		l_scoop = scoops[0]; items << Item{ l_scoop, l_scoop_rect,0};
-		c_scoop = scoops[1]; items << Item{ c_scoop, c_scoop_rect,1 };
-		r_scoop = scoops[2]; items << Item{ r_scoop, r_scoop_rect,2 };
+		l_scoop = scoops[0]; items << Item{ l_scoop, l_scoop_rect,false,0 };
+		c_scoop = scoops[1]; items << Item{ c_scoop, c_scoop_rect,false,1 };
+		r_scoop = scoops[2]; items << Item{ r_scoop, r_scoop_rect,false,2 };
 
 	}
 
@@ -145,15 +144,18 @@ public:
 			if (x.rect.leftClicked())
 			{
 
-				if (isChoiseitem)
+				if (x.ok == true)
 				{
 					isChoiseitem = false;
+					x.ok = false;
+					nowindex = 99;
 				}
 
 				else {
 					nowitem = x.scoop;
-					nowindex = x.index;
+					x.ok = true;
 					isChoiseitem = true;
+					nowindex = x.nowindex;
 				}
 				//Print << nowitem.scoop_description;
 			}
@@ -173,25 +175,48 @@ public:
 					m_x = Min(x, m_width - nowitem.lenth);
 					m_y = Min(y, m_width - nowitem.lenth);
 					//スコップがマップの範囲外になったときに、参照をずらす
-					
-					const RectF scooprect{ (Point{Min(x,m_width - nowitem.lenth)*m_size,Min(y,m_height - nowitem.lenth)*m_size}+Offset),m_size };
+
+					const RectF scooprect{ (Point{Min(x,m_width - nowitem.lenth) * m_size,Min(y,m_height - nowitem.lenth) * m_size} + Offset),m_size };
 
 
 					//掘る範囲が決定されたとき
 					if (rect.leftClicked())
 					{
+						int32 countall = 0;
+						int32 countone = 100;
 						//配列の値を変える
-						//二重ループの中に二重ループするのやめたい
+
 						for (int32 i = m_y; i < m_y + nowitem.lenth; i++) {
 							for (int32 j = m_x; j < m_x + nowitem.lenth; j++) {
 
-								
-								getData().sandmap[i][j]++;
+								//周囲のマスを調べて、深さの差が2以上であるなら、掘り進めることはできない（無限に掘れる）
+								//今から実装する
+
+								countall += getData().sandmap[i][j];
+								countone = Min(countone, getData().sandmap[i][j]);
+							}
+						}
+						bool overlap = false;
+						if (countall / (nowitem.lenth) == countone) {
+							overlap = true;
+						}
+						for (int32 i = m_y; i < m_y + nowitem.lenth; i++) {
+							for (int32 j = m_x; j < m_x + nowitem.lenth; j++) {
+								getData().sandmap[i][j] = countone + true;
 								m_score += 10 * Pow(getData().sandmap[i][j], 2);
 							}
 						}
+
 						m_turn--;
-						items[nowindex] = Item(scoops.choice(), items[nowindex].rect, nowindex);
+						int32 indexcount = 0;
+						for (auto& x : items)
+						{
+							if (x.ok = true) {
+
+								items[indexcount] = Item(scoops.choice(), items[indexcount].rect, false);
+							}
+							indexcount++;
+						}
 					}
 
 
@@ -201,7 +226,7 @@ public:
 		}
 
 		//turn終了したらランキングへ
-		if(m_turn<=0)
+		if (m_turn <= 0)
 		{
 			// ランキング画面へ
 			changeScene(State::Ranking);
@@ -216,17 +241,20 @@ public:
 	//描写
 	void draw() const override
 	{
+		//背景
+		Scene::Rect().draw(Arg::top = ColorF{ 0.2, 0.5, 1.0 }, Arg::bottom = ColorF{ 0.5, 0.8, 1.0 });
+
 		//砂マップ
 		for (int32 y = 0; y < m_height; y++) {
 			for (int32 x = 0; x < m_width; x++) {
 				const RectF rect{ (Point{ (x * m_size), (y * m_size)} + Offset), m_size };
 				const ColorF color{ (3 - getData().sandmap[y][x]) / 3.0 };
 				rect.stretched(-1).draw(color);
-				if ((isChoiseitem==true) && rect.mouseOver())
+				if ((isChoiseitem == true) && rect.mouseOver())
 				{
 					rect.drawFrame(8, 0);
-					
-					const RectF scooprect{ (Point{Min(x,m_width - nowitem.lenth)*m_size,Min(y,m_height - nowitem.lenth)*m_size}+Offset),m_size*nowitem.lenth };
+
+					const RectF scooprect{ (Point{Min(x,m_width - nowitem.lenth) * m_size,Min(y,m_height - nowitem.lenth) * m_size} + Offset),m_size * nowitem.lenth };
 					scooprect.draw(color);
 
 				}
@@ -238,36 +266,41 @@ public:
 		inv_rect.rounded(10).draw(inv_color);
 		inv_rect.rounded(10).drawFrame(3, 3, ColorF{ 0.5 });
 
-		//スコップ背景
+		//スコップ
 		for (auto& x : items) {
 			x.rect.draw(Palette::Whitesmoke);
+			if (x.ok)
+			{
+				x.rect.drawFrame(5, 0);
+			}
+			x.scoop.texture.draw(x.rect.x + 20, x.rect.y + 20);
 		}
 
 		//アイテム説明
 		//選択アイテムの説明をする
 		if (isChoiseitem)
 		{
-				FontAsset(U"Menu")(nowitem.description).drawAt(30, Vec2{ 1000, 600 });
-				//Print << nowitem.scoop_description;
-			
+			FontAsset(U"Text")(nowitem.description).drawAt(30, Vec2{ 1000, 600 });
+			//Print << nowitem.scoop_description;
+
 		}
 
-		//マウスカーソルが重なったアイテムの説明もする（動かない）
-		for(auto& x: items)
+		//マウスカーソルが重なったアイテムの説明もする
+		for (auto& x : items)
 		{
-			if ((isChoiseitem==false) && x.rect.mouseOver())
+			if ((isChoiseitem == false) && x.rect.mouseOver())
 			{
-				FontAsset(U"Menu")(x.scoop.description).drawAt(30, Vec2{ 1000, 600 });
+				FontAsset(U"Text")(x.scoop.description).drawAt(30, Vec2{ 1000, 600 });
 				//Print << nowitem.scoop_description;
 			}
 		}
 
 		//残りターン数の表示
-		FontAsset(U"Menu")(U"日暮れまであと\n{}ターン"_fmt(m_turn)).drawAt(30, Vec2{1000,80});
+		FontAsset(U"Text")(U"日暮れまであと\n{}ターン"_fmt(m_turn)).drawAt(30, Vec2{ 1000,80 });
 
 		//スコアの表示
-		FontAsset(U"Menu")(U"現在のスコア　{}"_fmt(m_score)).drawAt(30, Vec2{1000,80});
-		
+		FontAsset(U"Text")(U"現在のスコア　{}"_fmt(m_score)).drawAt(30, Vec2{ 1000,150 });
+
 	}
 
 private:
@@ -301,13 +334,11 @@ private:
 		Triangle,
 	};
 
-
-
 	struct Scoop {
 		Scoopname name;       // 砂の名前
-		ColorF color;       // 砂の色
+		Texture texture;       // 砂の色
 		String description;// 砂の説明文
-		int32 lenth;   // 砂の硬さ（掘るのに必要な労力などに影響するかも）
+		int32 lenth = 0;   // 砂の硬さ（掘るのに必要な労力などに影響するかも）
 	};
 
 	int32 scoop_size = 4;
@@ -319,11 +350,13 @@ private:
 
 	//今選んでるアイテム
 	Scoop nowitem;
-	//今のインデックス　これ絶対ダメなやつ
+	//今選んでるのがどれか
 	int32 nowindex;
 
 	//アイテムを選んでいるか
-	bool isChoiseitem=false;
+	bool isChoiseitem = false;
+	//アイテムが選ばれているか
+	bool isCoosen = false;
 
 	//アイテム欄のスコップ3種類
 
@@ -331,145 +364,30 @@ private:
 	{
 		Scoop scoop;
 		RectF rect;
-		int32 index;
+		bool ok;
+		int32 nowindex;
 	};
 
 	//アイテムの配列
 	Array<Item> items;
 
 	Scoop l_scoop, c_scoop, r_scoop;
-	const RectF l_scoop_rect{ Arg::center(150,650),90 };
-	const RectF c_scoop_rect{ Arg::center(300,650),90 };
-	const RectF r_scoop_rect{ Arg::center(450,650),90 };
+	const RectF l_scoop_rect{ Arg::center(150,650),85 };
+	const RectF c_scoop_rect{ Arg::center(300,650),85 };
+	const RectF r_scoop_rect{ Arg::center(450,650),85 };
 
 	//アイテム説明欄
 	const RectF discription{ Arg::center(800,600),100 };
 
 
 	//ターン数
-	int32 m_turn=25;
+	int32 m_turn = 15;
 
 	//ゲームのスコア
 	int32 m_score = 0;
+
 };
 
-class Game : public App::Scene
-{
-public:
-
-	Game(const InitData& init)
-		: IScene{ init }
-	{
-		// 横 (Scene::Width() / blockSize.x) 個、縦 5 個のブロックを配列に追加する
-		for (auto p : step(Size{ (Scene::Width() / BrickSize.x), 5 }))
-		{
-			m_bricks << Rect{ (p.x * BrickSize.x), (60 + p.y * BrickSize.y), BrickSize };
-		}
-	}
-
-	void update() override
-	{
-		// ボールを移動
-		m_ball.moveBy(m_ballVelocity * Scene::DeltaTime());
-
-		// ブロックを順にチェック
-		for (auto it = m_bricks.begin(); it != m_bricks.end(); ++it)
-		{
-			// ブロックとボールが交差していたら
-			if (it->intersects(m_ball))
-			{
-				// ボールの向きを反転する
-				(it->bottom().intersects(m_ball) || it->top().intersects(m_ball)
-					? m_ballVelocity.y : m_ballVelocity.x) *= -1;
-
-				// ブロックを配列から削除（イテレータが無効になるので注意）
-				m_bricks.erase(it);
-
-				AudioAsset(U"Brick").playOneShot(0.5);
-
-				// スコアを加算
-				++m_score;
-
-				// これ以上チェックしない
-				break;
-			}
-		}
-
-		// 天井にぶつかったらはね返る
-		if (m_ball.y < 0 && m_ballVelocity.y < 0)
-		{
-			m_ballVelocity.y *= -1;
-		}
-
-		// 左右の壁にぶつかったらはね返る
-		if ((m_ball.x < 0 && m_ballVelocity.x < 0)
-			|| (Scene::Width() < m_ball.x && 0 < m_ballVelocity.x))
-		{
-			m_ballVelocity.x *= -1;
-		}
-
-		// パドルにあたったらはね返る
-		if (const Rect paddle = getPaddle();
-			(0 < m_ballVelocity.y) && paddle.intersects(m_ball))
-		{
-			// パドルの中心からの距離に応じてはね返る方向を変える
-			m_ballVelocity = Vec2{ (m_ball.x - paddle.center().x) * 10, -m_ballVelocity.y }.setLength(Speed);
-		}
-
-		// 画面外に出るか、ブロックが無くなったら
-		if ((Scene::Height() < m_ball.y) || m_bricks.isEmpty())
-		{
-			// ランキング画面へ
-			changeScene(State::Ranking);
-
-			getData().lastGameScore = m_score;
-		}
-	}
-
-	void draw() const override
-	{
-		Scene::SetBackground(ColorF{ 0.2 });
-
-		// すべてのブロックを描画する
-		for (const auto& brick : m_bricks)
-		{
-			brick.stretched(-1).draw(HSV{ brick.y - 40 });
-		}
-
-		// ボールを描く
-		m_ball.draw();
-
-		// パドルを描く
-		getPaddle().draw();
-
-		FontAsset(U"GameScore")(m_score).draw(10, 10);
-	}
-
-private:
-
-	// ブロックのサイズ
-	static constexpr Size BrickSize{ 40, 20 };
-
-	// ボールの速さ
-	static constexpr double Speed = 480.0;
-
-	// ボールの速度
-	Vec2 m_ballVelocity{ 0, -Speed };
-
-	// ボール
-	Circle m_ball{ 400, 400, 8 };
-
-	// ブロックの配列
-	Array<Rect> m_bricks;
-
-	// 現在のゲームのスコア
-	int32 m_score = 0;
-
-	Rect getPaddle() const
-	{
-		return{ Arg::center(Cursor::Pos().x, 500), 60, 10 };
-	}
-};
 
 // ランキングシーン
 class Ranking : public App::Scene
@@ -557,13 +475,14 @@ void Main()
 	//タイトルフォントの設定
 	FontAsset::Register(U"TitleFont", FontMethod::MSDF, 50, U"NewAssets/font/JF-Dot-MPlus12B.ttf");
 	FontAsset(U"TitleFont").setBufferThickness(4);
+	FontAsset::Register(U"Text", FontMethod::MSDF, 20, U"NewAssets/font/JF-Dot-MPlus12B.ttf");
 	FontAsset::Register(U"Menu", FontMethod::MSDF, 40, Typeface::Medium);
 	FontAsset::Register(U"Ranking", 40, Typeface::Heavy);
 	FontAsset::Register(U"GameScore", 30, Typeface::Light);
 	AudioAsset::Register(U"Brick", GMInstrument::Woodblock, PianoKey::C5, 0.2s, 0.1s);
 
 	//曲の設定
-	AudioAsset::Register(U"BGM", Audio::Stream, U"example/test.mp3");
+	AudioAsset::Register(U"BGM", Audio::Stream, U"NewAssets/buckmusic.wav", Loop::Yes);
 	double volume = 1.0;
 
 	//わけわかめ　もうなにもわからない
@@ -574,11 +493,10 @@ void Main()
 	// タイトルシーンを登録
 	manager.add<Title>(State::Title);
 	manager.add<SandCastle>(State::SandCastle);
-	manager.add<Game>(State::Game);
 	manager.add<Ranking>(State::Ranking);
 
 	// "Game" シーンから開始
-	manager.init(State::SandCastle);
+	//manager.init(State::SandCastle);
 
 	while (System::Update())
 	{
